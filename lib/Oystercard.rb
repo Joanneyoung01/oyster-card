662c5fa6f2cec2621c558adc6e91ff67df72247b
Joanne Young <joanneyoung_@hotmail.com>
2020-04-21 15:44:13 +0100

    deduct fare, journey status, touch in and out methods added

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,13 +1,32 @@
 class Oystercard
 
   attr_reader :balance
+  MAXIMUM_BALANCE = 90
 
   def initialize
     @balance = 0
+    @journey_status = false
   end
 
   def top_up(value)
-   @balance = @balance + value
+    raise "Upper limit of #{MAXIMUM_BALANCE} reached!" if value + @balance > MAXIMUM_BALANCE
+   @balance += value
+  end
+
+  def deduct(fare)
+    @balance -= fare
+  end
+
+  def in_journey?
+    @journey_status
+  end
+
+  def touch_in
+    @journey_status = true
+  end
+
+  def touch_out
+    @journey_status = false
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,5 +13,35 @@ describe Oystercard do
   it 'can top up the balance' do
     expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
   end
+  
+  it 'has an upper limit' do
+    limit = Oystercard::MAXIMUM_BALANCE
+    subject.top_up(limit)
 
+    expect { subject.top_up(5) }.to raise_error "Upper limit of #{limit} reached!"
+  end
+
+  it 'can deduct fare' do
+    expect(subject).to respond_to(:deduct).with(1).argument
+  end
+
+  it 'should deduct fare from balance' do
+    subject.top_up(10)
+    expect(subject.deduct(5)). to eq 5
+  end
+
+  it 'is initially not in journey' do
+    expect(subject).not_to be_in_journey
+  end
+
+  it 'can touch in' do
+    subject.touch_in
+    expect(subject).to be_in_journey
+  end
+
+  it 'can touch out' do
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_journey
+  end
 end
